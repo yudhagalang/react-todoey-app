8b24d04d73c80f1901d06e01758613219eb98296
yudhagalang <yudhagalang@gmail.com>
2020-03-29 17:39:19 +0700

    first-commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Header = () => {
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "2em"
+  };
+  return (
+    <header style={headerStyle}>
+      <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
+        Simple Todoey App
+      </h1>
+      <p style={{ fontSize: "19px" }}>
+        Please add to-doeys item(s) through the input field
+      </p>
+    </header>
+  );
+};
+export default Header;
