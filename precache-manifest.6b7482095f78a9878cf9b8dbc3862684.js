ba9423534b8c3cb4354cbd94ccd9f3f788812eaf
yudhagalang <yudhagalang@gmail.com>
2020-03-29 17:47:17 +0700

    Updates

---
+++ precache-manifest.6b7482095f78a9878cf9b8dbc3862684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c8178dd6cd1de3094ff285aba8f2b4",
+    "url": "/react-todoey-app/index.html"
+  },
+  {
+    "revision": "eda6225cfb3b5daaa4d9",
+    "url": "/react-todoey-app/static/css/main.bba6b7e0.chunk.css"
+  },
+  {
+    "revision": "dcb78e13ef3b0714c7ce",
+    "url": "/react-todoey-app/static/js/2.2c029cee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoey-app/static/js/2.2c029cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda6225cfb3b5daaa4d9",
+    "url": "/react-todoey-app/static/js/main.c290d274.chunk.js"
+  },
+  {
+    "revision": "a4dafa94d8a42737dc0e",
+    "url": "/react-todoey-app/static/js/runtime-main.00bd7334.js"
+  }
+]);
